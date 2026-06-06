d49dc7e1abe86b6de4c4cd7435dddc39a6403829
Welepy-dev <mwsilva.contato@gmail.com>
2026-06-06 14:15:10 +0100

    ok this needs to be better thought

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from api.constants import BUNDESLIGA_ID
-from api.endpoints import get_all_matches, get_matches
+from api.constants import LALIGA_ID, UCL_ID
+from api.endpoints import get_all_matches
+from models.fixture import parse_fixtures
 
 def main():
-    print(get_all_matches())
+    fixtures = parse_fixtures(get_all_matches())
+    for fixture in fixtures:
+        if fixture.competition_id == UCL_ID and fixture.homeTeam == 'Barça' or fixture.awayTeam == 'Barça' and fixture.competition_id != LALIGA_ID:
+            print(fixture.score)
 
 if __name__ == "__main__":
     main()
