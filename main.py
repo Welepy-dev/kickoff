a12dad1316683cb34e22a0b627b12450ec979cb7
Welepy-dev <mwsilva.contato@gmail.com>
2026-06-08 20:27:38 +0100

    sorting games to put on fixtures

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from api.constants import LALIGA_ID, UCL_ID
 from api.endpoints import get_all_matches
 from models.fixture import parse_fixtures
 
 def main():
-    fixtures = parse_fixtures(get_all_matches())
-    for fixture in fixtures:
-        if fixture.competition_id == UCL_ID and fixture.homeTeam == 'Barça' or fixture.awayTeam == 'Barça' and fixture.competition_id != LALIGA_ID:
-            print(fixture.score)
+    raw = get_all_matches()
+    fixtures = parse_fixtures(raw)
+
+    # Sort by date ascending (soonest first)
+    # fixtures.sort(key=lambda f: f.date)
+
+    fixtures.sort(key=lambda f: f.date, reverse=True)
+
+    for f in fixtures:
+        print(f"Game: {f.name}, date: {f.date}")
 
 if __name__ == "__main__":
     main()
