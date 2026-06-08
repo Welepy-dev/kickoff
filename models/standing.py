a57ca6bbab05aa26da6e29974ca205233485e877
Welepy-dev <mwsilva.contato@gmail.com>
2026-06-08 14:53:53 +0100

    parse standings for my dataclass

--- models/standing.py
+++ models/standing.py
@@ -15,6 +15,30 @@ class Standing:
     points: int
     position: int
 
-def parse_standings(data: list) -> list[Standing]:
-    print(data)
-    return []
+
+def parse_standings(data: list | dict) -> list[Standing]:
+    if isinstance(data, dict):
+        data = [data]
+    standings = []
+    for item in data:
+        for table in item["standings"]:
+            if table["type"] != "TOTAL":
+                continue
+            for row in table["table"]:
+                standings.append(
+                    Standing(
+                        team=row["team"]["shortName"],
+                        team_id=row["team"]["id"],
+                        competition=item["competition"]["name"],
+                        competition_id=item["competition"]["id"],
+                        playedGames=row["playedGames"],
+                        wins=row["won"],
+                        draws=row["draw"],
+                        losses=row["lost"],
+                        goalDifference=row["goalDifference"],
+                        goalsFor=row["goalsFor"],
+                        points=row["points"],
+                        position=row["position"],
+                    )
+                )
+    return standings
