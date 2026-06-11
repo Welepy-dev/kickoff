f03c6a1b9e8f3eb9aa78168df8261bb4edb944ae
Welepy-dev <mwsilva.contato@gmail.com>
2026-06-11 18:07:42 +0100

    Refactor main.py and app.py to improve match data handling; update FixtureWidget for better UI composition

--- ui/app.py
+++ ui/app.py
@@ -1,21 +1,51 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Label, Tabs, Tab, ContentSwitcher
+from textual.widgets import Label, Tabs, Tab, ContentSwitcher, ListView, ListItem
 from textual.containers import Vertical, Horizontal
 
+from api.endpoints import get_all_matches, get_all_top_scorers
+from models.fixture import parse_fixtures
+from models.scorer import parse_scorers
+from ui.FixtureWidget import FixtureWidget
 
 class UI(App):
-    
+    scorers = parse_scorers(get_all_top_scorers())
+
+
+
     CSS_PATH = "app.css"
 
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
+        matches = parse_fixtures(get_all_matches())
+        match = matches[0]
         yield Horizontal (
             Vertical (
-                Label ("Fixtures"),
+                Label("Fixtures"),
                 Tabs (
                     Tab("Next", id="firstTab"),
                     Tab("Previous", id="secondTab"),
+                ),
+                ContentSwitcher(
+                    Vertical (
+                        ListView (
+                            ListItem(Label("Next placeholder 1")),
+                            ListItem(Label("Next placeholder 2")),
+                            ListItem(Label("Next placeholder 3")),
+                            FixtureWidget("data", "hora", match.homeTeam, match.homeTeamCrest, match.awayTeam, match.awayTeamCrest, True, match.score),
+                            id="next-list-1",
+                        ),
+                        id="firstTab",
+                    ),
+                    Vertical (
+                        ListView (
+                            ListItem(Label("Previous placeholder 1")),
+                            ListItem(Label("Previous placeholder 2")),
+                            ListItem(Label("Previous placeholder 3")),
+                            id="previous-list-1",
+                        ),
+                        id="secondTab",
+                    ),
                 ),
                 id="fixtures"
             ),
