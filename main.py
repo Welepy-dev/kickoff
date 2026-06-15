794864cc072430fec8ad00dce9da8cc59d5c333e
Welepy-dev <mwsilva.contato@gmail.com>
2026-06-15 17:50:29 +0100

    this main probably wont be changed anytime soon

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from api.endpoints import get_all_matches, get_all_top_scorers
-from models.fixture import parse_fixtures
-from models.scorer import parse_scorers
+from api.endpoints import get_all_matches
 from ui.app import UI
+from pprint import pprint
 
 def main():
-    matches = parse_fixtures(get_all_matches())
-    scorers = parse_scorers(get_all_top_scorers())
-
     ui = UI()
     ui.run()
 
