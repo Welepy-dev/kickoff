e10aae5667b2f4c2ed83223801eb2303b3649908
Welepy-dev <mwsilva.contato@gmail.com>
2026-06-18 19:31:24 +0100

    adding top scorers, need to fix UI

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,8 @@
 from operator import attrgetter
 from datetime import datetime, timezone
-from api.endpoints import get_all_matches
+from api.endpoints import get_all_matches, get_all_top_scorers
 from models.fixture import parse_fixtures
+from models.scorer import parse_scorers
 from textual.app import App, ComposeResult
 from textual.widgets import Label, Tabs, Tab, ContentSwitcher, DataTable, LoadingIndicator
 from textual.containers import Vertical, Horizontal
@@ -34,15 +35,42 @@ class UI(App):
                 ),
                 id="fixtures"
             ),
-            Vertical(id="scorers"),
+            Vertical(
+                Label("Top Scorers"),
+                DataTable(id="scorers-table", cursor_type="row"),
+                id="scorers"
+            ),
             id="main"
         )
 
     def on_mount(self) -> None:
+        self.query_one("#scorers-table", DataTable).add_columns("Name", "Goals", "Team", "Assists")
         for table_id in ("next-table", "previous-table"):
             table = self.query_one(f"#{table_id}", DataTable)
-            table.add_columns("Date", "Home", "Score", "Away", "Competition")
+            table.add_columns("Date", "Home", "Score", "Away", "Competition", "Time")
         self.load_fixtures()
+        self.load_scorers()
+
+    @work(thread=True)
+    def load_scorers(self) -> None:
+        scorers = parse_scorers(get_all_top_scorers())
+        scorers = sorted(scorers, key=attrgetter('goals'), reverse=True)
+
+        scorer_rows = []
+
+        for scorer in scorers:
+            row = (
+                scorer.player,
+                scorer.goals,
+                scorer.team,
+                scorer.assists
+            )
+            scorer_rows.append(row)
+        self.app.call_from_thread(self._populate_scorers, scorer_rows)
+
+    def _populate_scorers(self, scorer_rows) -> None:
+        self.query_one("#scorers-table", DataTable).add_rows(scorer_rows)
+
 
     @work(thread=True)
     def load_fixtures(self) -> None:
@@ -62,6 +90,7 @@ class UI(App):
                 middle,
                 fixture.awayTeam or "",
                 fixture.competition or "",
+                str(dt.time()),
             )
             if fixture.fulltime or dt < now:
                 previous_rows.insert(0, row)
