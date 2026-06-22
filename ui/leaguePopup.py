e5fea9525835cea4e239e25a841d78721a5905c9
Welepy-dev <mwsilva.contato@gmail.com>
2026-06-22 19:55:47 +0100

    refactor LeaguesPopup to use generic type and remove unused button and methods

--- ui/leaguePopup.py
+++ ui/leaguePopup.py
@@ -3,25 +3,19 @@ from textual.screen import ModalScreen
 from textual.widgets import Button
 from textual.containers import Grid
 
-class LeaguesPopup(ModalScreen):
+class LeaguesPopup(ModalScreen[str]):
 
     def compose(self) -> ComposeResult:
         yield Grid(
             Button("Champions League"),
-            Button("Europa League"),
             Button("Premier League"),
             Button("Primeira Liga"),
             Button("World Cup"),
             Button("Eredivisie"),
             Button("Bundesliga"),
+            Button("Euro"),
             Button("Serie A"),
             Button("Ligue 1"),
             Button("Laliga"),
             id="leagueButtons"
         )
-    
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        self.dismiss(event.button.label)
-
-    def menu_result(self, result) -> None:
-        self.noify(f"Selected: {result}")
